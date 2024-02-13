a60d1cdeafbedd7c3fa4ef6764c5738970839b27
Guangjun Bao <gbao@cactusiot.com>
2024-02-13 13:06:19 +0100

    feat: build project

--- buf-go-angular-example/webapp/src/app/app.component.ts
+++ buf-go-angular-example/webapp/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonService } from '../gen/common/v1/person_connect';
+import { ObservableClient } from '../connect/observable-client';
+import { Person } from '../gen/common/v1/person_pb';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'webapp';
+
+  constructor(
+    @Inject(PersonService)
+    private client: ObservableClient<typeof PersonService>
+  ) {
+    var person = new Person()
+    person.name = "pepe"
+    person.email = "example@example"
+    this.client.create(new Person())
+  }
 }
--- buf-go-angular-example/webapp/src/app/app.config.ts
+++ buf-go-angular-example/webapp/src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+ import { providerTransport } from '../connect/transport_provider';
+import { provideClient } from '../connect/client_provider';
+import { PersonService } from '../gen/common/v1/person_connect';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    providerTransport({
+      baseUrl: "http://localhost:8080"
+    }),
+    provideClient(PersonService)
+  ]
 };
