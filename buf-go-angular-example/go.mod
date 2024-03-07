5a95d295c1863a04c8acf8de24cb324df4455dc3
Guangjun Bao <gbao@cactusiot.com>
2024-03-07 11:40:38 +0100

    feat: commit all file

--- buf-go-angular-example/go.mod
+++ buf-go-angular-example/go.mod
@@ -4,6 +4,8 @@ go 1.21.6
 
 require (
 	connectrpc.com/connect v1.14.0
+	connectrpc.com/cors v0.1.0
+	github.com/rs/cors v1.10.1
 	golang.org/x/net v0.17.0
 	google.golang.org/protobuf v1.32.0
 )
--- buf-go-angular-example/main.go
+++ buf-go-angular-example/main.go
@@ -4,8 +4,10 @@ import (
 	commonv1 "buf-go-angular-example/gen/common/v1"
 	"buf-go-angular-example/gen/common/v1/commonv1connect"
 	"connectrpc.com/connect"
+	connectcors "connectrpc.com/cors"
 	"context"
 	"fmt"
+	"github.com/rs/cors"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"net/http"
@@ -16,13 +18,25 @@ const address = "localhost:8080"
 func main() {
 	mux := http.NewServeMux()
 	path, handler := commonv1connect.NewPersonServiceHandler(&personServiceServer{})
-	mux.Handle(path, handler)
+	handlerWithCors := withCORS(handler)
+	mux.Handle(path, handlerWithCors)
 	fmt.Println("... Listening on", address)
 	http.ListenAndServe(
 		address,
 		// Use h2c so we can serve HTTP/2 without TLS.
 		h2c.NewHandler(mux, &http2.Server{}),
 	)
+}
+
+// withCORS adds CORS support to a Connect HTTP handler.
+func withCORS(h http.Handler) http.Handler {
+	middleware := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: connectcors.AllowedMethods(),
+		AllowedHeaders: connectcors.AllowedHeaders(),
+		ExposedHeaders: connectcors.ExposedHeaders(),
+	})
+	return middleware.Handler(h)
 }
 
 type personServiceServer struct {
